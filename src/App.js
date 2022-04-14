0991d998622ce1c650f90257d2ee7a49e94a2291
hannabananah <hanna0175@gmail.com>
2022-04-14 12:12:24 +0900

    04 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-function Fruit({fav}){
-  return <h1>I like {fav}</h1>;
+function Fruit({ what, picture, rating }){
+  return (
+    <div>
+      <img src={picture} alt={what} />
+      <h2>I like {what}</h2>
+      <h4>{rating} / 5.0 </h4>
+      <hr/>
+      <br/>
+    </div>
+  );
 }
+
+const fruitLike=[
+  {
+    id: 1,
+    name: '바나나',
+    image: 'http://qwerew.cafe24.com/images/banana.png',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: '오렌지',
+    image: 'http://qwerew.cafe24.com/images/orange.png',
+    rating: 4.2,
+  },
+  {
+    id: 3,
+    name: '사과',
+    image: 'http://qwerew.cafe24.com/images/apple.png',
+    raging: 4.8,
+  },
+  {
+    id: 4,
+    name: '메론',
+    image: 'http://qwerew.cafe24.com/images/melon.jpg',
+    rating: 3.7,
+  },
+];
 
 function App() {
   return (
-  <div>
-    <h1>
-      <p>안녕하세요!</p>
-      </h1>
-      <Fruit fav="coconuts" />
-      <Fruit fav="avocados" />
-      <Fruit fav="mangoes" />
-      <Fruit fav="dragon fruits" />
-  </div> 
-    );
+    <div>
+      {fruitLike.map((dish) => (
+        <Fruit key={dish.id} what={dish.name} picture={dish.image} rating={dish.rating} />
+      ))}
+    </div>
+  );
 }
+Fruit.propTypes = {
+  what: propTypes.string.isRequired,
+  picture: propTypes.string.isRequired,
+  rating: propTypes.number.isRequired,
+};
 
 export default App;
