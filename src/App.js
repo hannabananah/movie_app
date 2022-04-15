648d102b4a099452ffb3f2b1e24675907622ee9d
hannabananah <hanna0175@gmail.com>
2022-04-15 11:54:02 +0900

    05 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
-import React from 'react';
-import propTypes from 'prop-types';
+import React from "react"
 
-function Fruit({ what, picture, rating }){
-  return (
-    <div>
-      <img src={picture} alt={what} />
-      <h2>I like {what}</h2>
-      <h4>{rating} / 5.0 </h4>
-      <hr/>
-      <br/>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		isLoading: true,
+		movies:[]
+	}
+
+	componentDidMount(){
+		setTimeout(()=>{
+			this.setState({isLoading: false,})
+		},5000)
+	}
+	render() {
+		const { isLoading } = this.state;
+		
+		return (
+		<div>
+			{isLoading ? '로딩중...' : '로딩완료'}
+		</div>
+		)
+	}
 }
 
-const fruitLike=[
-  {
-    id: 1,
-    name: '바나나',
-    image: 'http://qwerew.cafe24.com/images/banana.png',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: '오렌지',
-    image: 'http://qwerew.cafe24.com/images/orange.png',
-    rating: 4.2,
-  },
-  {
-    id: 3,
-    name: '사과',
-    image: 'http://qwerew.cafe24.com/images/apple.png',
-    raging: 4.8,
-  },
-  {
-    id: 4,
-    name: '메론',
-    image: 'http://qwerew.cafe24.com/images/melon.jpg',
-    rating: 3.7,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {fruitLike.map((dish) => (
-        <Fruit key={dish.id} what={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
-}
-Fruit.propTypes = {
-  what: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number.isRequired,
-};
-
-export default App;
+export default App
