a94e1e2f658c20e15034cd13ffa5b4c4911ffdf2
hannabananah <hanna0175@gmail.com>
2022-04-18 12:50:59 +0900

    06-1 깃허브에 리액트 앱 업로드하기- Movie 컴포넌트생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from "react"
+import React from 'react';
+import axios from "axios";
 
 class App extends React.Component {
-	state = {
-		isLoading: true,
-		movies:[]
-	}
+  state={
+    isLoading:true,
+    movies:[]
+  }
+  getMovies= async () => {
+    const {
+      data: {
+        data: { movies},
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+    this.setState({ movies , isLoading: false})
+  }
 
-	componentDidMount(){
-		setTimeout(()=>{
-			this.setState({isLoading: false,})
-		},5000)
-	}
-	render() {
-		const { isLoading } = this.state;
-		
-		return (
-		<div>
-			{isLoading ? '로딩중...' : '로딩완료'}
-		</div>
-		)
-	}
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        isLoading: false,
+      })
+    },5000)
+    this.getMovies();
+  }
+  render(){
+    const { isLoading } = this.state
+    
+    return(
+      <div>
+        {isLoading ? '로딩중...' : '로딩완료'}
+      </div>
+    )
+  }
 }
-
-export default App
+export default App;
