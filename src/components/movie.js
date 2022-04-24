93719b3b2306709af713293d5df4c2d396b060c1
hannabananah <hanna0175@gmail.com>
2022-04-24 18:32:11 +0900

    final completion code

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,34 +1,36 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./Movie.css";
-import {Link} from "react-router-dom"; 
+import React from "react"
+import PropTypes from "prop-types"
+import "./Movie.css"
+import {Link} from "react-router-dom";
 
-
+function Movie({ title, year, summary, poster, genres }) {
+	return (
+		<div className="movie__data">
+			<Link to={{ pathname:"/movie-detail", state:{year,title,summary,poster,genres}}}>
+				<img src={poster} alt={title} title={title} />
+				<h3 className="movie__title">{title}</h3>
+				<h5 className="movie__year">{year}</h5>
+				<ul className="movie__genres">
+					{genres.map((genre, index) => {
+						return (
+							<li key={index} className="movie_genre">
+								{genre}
+							</li>
+						)
+					})}
+				</ul>
+				<p className="movie__summary">{summary.slice(0,180)}...</p>
+			</Link>
+		</div>
+	)
+}
 //title,summary,medium_cover_image->poster
-function Movies({ title, year, summary, poster, genres }) {
-    return (
-    <div className="movie_data">
-        <Link to={{pathname:"/movie-detail",state:{year,title,summary,poster,genres}}}>
-          <img src={poster} alt={title} title={title}></img>
-          <h3 className="movie_title">{title}</h3>
-          <h5 className="movie_year">{year}</h5>
-          <ul className="movie_genres">
-            {genres.map((genre,index)=>{
-              return <li key={index} className="movie_genre">{genre}</li>
-            })}
-          </ul>
-          <p className="movie_summary">{summary.slice(0,180)}...</p>
-        </Link>
-    </div>
-    );
-  }
-  Movies.propTypes = {
-    year: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-  };
-  
-  export default Movies;
-  
+Movie.propTypes = {
+	year: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	summary: PropTypes.string.isRequired,
+	poster: PropTypes.string.isRequired,
+	genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Movie
