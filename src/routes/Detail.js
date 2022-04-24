136dc66e8b3baad36290fde0dbfcb9cb19a8e86f
hannabananah <hanna0175@gmail.com>
2022-04-24 18:10:04 +0900

    final completion code

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./Detail.css";
+import React from 'react';
+import './Detail.css';
 
 class Detail extends React.Component {
    componentDidMount() {
@@ -8,31 +8,27 @@ class Detail extends React.Component {
          history.push('/');
       }
    }
+
    render() {
-      const {location}=this.props
+      const {location}=this.props;
       if(location.state) {
          return (
             <div className="detail">
-               <img src={location.state.poster}
-                  alt={location.state.title} title={location.state.poster} />
-               <div className="movie_data">
-                  <h3 className="movie_title">{location.state.title}</h3>
-                  <h5 className="movie_year">{location.state.year}</h5>
-                  <ul className="movie_genres">
+               <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+               <div className="movie__data">
+                  <h3 className="movie__title">{location.state.title}</h3>
+                  <h5 className="movie__year">{location.state.year}</h5>
+                  <ul className="movie__genres">
                      {location.state.genres.map((genre, index) => {
-                        return (<li key={index} className="movie_genre">
-                           {location.state.genre}
-                        </li>
-                        )
+                        return <li key={location.state.index} className="movie_genre">{location.state.genre}</li>;
                      })}
                   </ul>
-                  <p className="movie_summary">{location.state.summary}</p>
+                  <p className="movie__summary">{location.state.summary}</p>
                </div>
-               <span>{location.state.title}</span>
             </div>
          );
       } else {
-         return null
+         return null;
       }
    }
 }
